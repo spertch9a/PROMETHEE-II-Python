9d319c1cb57815e1daf7187bb06fbaa72972b0e3
Fortas Oussama Ilyes <fortasoussamailyes@gmail.com>
2021-02-12 15:48:16 +0100

    Defining step 1

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Authon Oussama FORTAS
+"""
+
+print("PROMETHEE 2 METHOD")
+print("#######################################################")
+
+print("We will be using AHP : Analytic Hierarchy Process.")
+
+#We'll use the mobile choosing example, 
+"""
+The criterias to choose upon are 
+(Weightage= 0.35) Price or Cost 
+(Weightage=0.25 ) Storage space
+(Weightage=0.25 ) Camera
+(Weightage=0.15 ) Looks =  {Excellent : 5 , Good : 4 , Average : 3  , Below Average :2 , Low : 1}
+"""
+
+print("Full Ranking")
+
+#Step 1 : Normalize the Evaluation Matrix (decision matrix) 
+print("STEP 1 : Normalize the Evaluation Matrix")
+"""
+Captures / Formula for beneficial and non beneficial criteria
+Beneficial are direct categories
+Non beneficial are indirect ones
+For each criteria we need to calculate the maximum and minimum for each criteria
+"""
