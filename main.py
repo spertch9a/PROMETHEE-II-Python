85026e9046ea6fdc10c1f4f06bb4401259a4034e
Fortas Oussama Ilyes <fortasoussamailyes@gmail.com>
2021-02-15 02:19:49 +0100

    calculated maxmin of every column

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ For each criteria we need to calculate the maximum and minimum for each criteria
 npmatrix  = np.array(Matrix)
 subnpmatix = npmatrix[1:,1:]
 print("npmatrix ")
+
 maxprice= 0  
 maxstorage= 0
 maxcamera = 0
@@ -54,16 +55,69 @@ minprice = 1000
 minstorage = 1000
 mincamera = 1000
 minlooks = 1000
-
-def maxminmatrix(matrix) :
+y = subnpmatix.astype(np.float)
+def maxminprice(matrix) :
     maxprice = 0
-    minprice  = 0
+    minprice  = 1000
     for row in range(len(matrix)):
-     if(int(maxprice) < int(matrix[row][0])) : 
+     if(maxprice < matrix[row][0]) : 
         maxprice = matrix[row][0]
      if(minprice > matrix[row][0]) :     
-        minprice= matrix[row][0]
-    return maxprice
+        minprice = matrix[row][0]
+
+    return maxprice, minprice
+def maxminstorage(matrix) :
+    maxstorage = 0
+    minstorage  = 1000
+    for row in range(len(matrix)):
+     if(maxstorage < matrix[row][1]) : 
+        maxstorage = matrix[row][1]
+     if(minstorage > matrix[row][1]) :     
+        minstorage = matrix[row][1]
+
+    return maxstorage, minstorage
+
+def maxmincamera(matrix) :
+    maxcamera = 0
+    mincamera  = 1000
+    for row in range(len(matrix)):
+     if(maxcamera < matrix[row][2]) : 
+        maxcamera = matrix[row][2]
+     if(mincamera > matrix[row][2]) :     
+        mincamera = matrix[row][2]
+
+    return maxcamera, mincamera
+
+def maxminlooks(matrix) :
+    maxlooks = 0
+    minlooks  = 1000
+    for row in range(len(matrix)):
+     if(maxlooks < matrix[row][3]) : 
+        maxlooks = matrix[row][3]
+     if(minlooks > matrix[row][3]) :     
+        minlooks = matrix[row][3]
+
+    return maxlooks, minlooks
 
 
-print("maxprice : ", maxminmatrix(subnpmatix))
+
+print("maxminprice : ", maxminprice(y))
+print("maxminstorage : ", maxminstorage(y))
+print("maxmincamera : ", maxmincamera(y))
+print("maxminlooks : ", maxminlooks(y))
+
+#STEP 2 :  calcule des difference 
+def calculeDiff():
+    #max_col  - min_c
+
+
+# https://www.youtube.com/watch?v=xe2XgGrI0Sg&t=55s
+
+
+
+
+
+
+
+
+
